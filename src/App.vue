7147aac51e1857210164ff720d11c5c6ebc4b527
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-08-06 13:30:35 +0200

    Refactor App.vue: Extracted navbar into separate component, removed inline styles, and updated template structure

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import ProductList from './components/ProductList'
-</script>
-
 <template>
-  <header class="bg-white">
-    <div class="wrapper">
-      <nav class="flex justify-between items-center w-[92%] mx-auto">
-        <router-link to="/">
-          <div>
-            <img class="w-16" src="../public/favicon.ico" alt="logo">
-          </div>
-        </router-link>
-        <div class="md:static absolute text-2xl text-black md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 ">
-          <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-            <li>
-              <router-link to="/">TakeItAll</router-link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <button class="flex items-center px-3 py-3 hover:bg-gray-200">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-            </svg>
-          </button>
-        </div>
-      </nav>
-    </div>
-  </header>
+  <div>
+    <!-- Render the Navbar component -->
+    <Navbar />
 
-  <template>
-    <div id="app">
-      <ProductList />
-    </div>
-  </template>
+    <!-- Render the content for the current route -->
+    <router-view />
+  </div>
 </template>
 
+<script>
+/**
+ * @fileoverview This is the main layout component for the application.
+ * It includes a Navbar and a router-view for rendering matched components based on the current route.
+ */
 
-<style scoped>
+import Navbar from './components/Navbar.vue';
 
-</style>
+/**
+ * Main layout component for the application.
+ * It includes the Navbar component and the router-view.
+ *
+ * @component
+ */
+export default {
+  components: {
+    /** 
+     * The Navbar component used for navigation throughout the app.
+     * @type {import('./components/Navbar.vue').default}
+     */
+    Navbar
+  }
+};
+</script>
